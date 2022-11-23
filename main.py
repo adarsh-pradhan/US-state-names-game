f4ab16597f22d0a1a2075cab64b7dcd84bfb1e1f
ADARSH PRADHAN <77764743+adarsh-pradhan@users.noreply.github.com>
2022-11-23 19:45:30 +0530

    Update main.py
    Added an exit functionality.

--- main.py
+++ main.py
@@ -12,6 +12,13 @@ guessed_states = []
 
 while len(guessed_states) < 50:
     answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?").title()
+    
+    if answer_state == "Exit";
+    missing_state = [state for state in all_states if state not in guessed_states]
+    new_data = pandas.DataFrame(missing_states)
+    new_data,to_csv("states_to_learn.csv")
+    break
+    
     if answer_state in data.state.to_list():
         guessed_states.append(answer_state)
         t = turtle.Turtle()
